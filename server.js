644d4e8a8167aedb13e32bd0ff0a241b00ef680a
JonathanTIngram <38962360+JonathanTIngram@users.noreply.github.com>
2021-02-19 01:06:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var express = require('express');
 var app = express();
 
 // Set up the server
-// process.env.PORT is related to deploying on heroku
+
 var server = app.listen(process.env.PORT || 3000, listen);
 
 // This call back just tells us that the server has started
